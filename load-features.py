5d4ecc6283701c5705887ec33ada2d591de3ae71
Lex Longhorn <emmanuelportes@outlook.com>
2025-08-05 19:03:21 -0400

    added raw data filters

--- load-features.py
+++ load-features.py
@@ -1,6 +1,9 @@
-from datetime import date, datetime
 import requests
-import os
+from datetime import date, datetime
+
+MIN_LATITUDE: float, MAX_LATITUDE = [-90.0, 90.0]
+MIN_MAGNITUDE: float, MAX_MAGNITUDE = [-1.0, 10.0]
+MIN_LONGITUDE: float, MAX_LONGITUDE = [-180.0, 180.0]
 
 def time_format(timestamp:int) -> date:
     timestamp = timestamp / 1000
@@ -14,16 +17,45 @@ def get_data(url: str) -> list[dict]:
     except requests.HTTPError:
         return "something went wrong"
     
-def main(url: str = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson') -> None:
-    features: list[dict] 
+def load_features(url: str) -> list:
+
+    features: list[list] 
     data: list[dict] = get_date(url)
 
+    for feature in data:
+        temp: list = []
+        featuretype: str = feature.get('type')
+        externalID: str = feature.get('id')
+        mag: float = feature.get('properties')['mag']
+        place: str = feature.get('properties')['place']
+        eventDate: date = time_format(feature.get('properties')['time'])
+        url: str = feature.get('properties')['url']
+        tsunami: bool = feature.get('properties')['tsunami']
+        magtype: str = feature.get('properties')['magType']
+        title: str = feature.get('properties')['title']
+        longitude: float, latitude: float, magnitude: float = feature.get('coordinates') 
 
+        if (longitude is None or latitude is None or magnitude is None or 
+                title is None or url is None or place is None magType is None ):
+            continue
 
-if __name__ == '__main__':
-    endtime: date = date.today().date()
-    URL: str = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&endtime={endtime}'
-    main()
+        if (MIN_LATITUDE > latitude > MAX_LATITUDE) or 
+                (MIN_MAGNITUDE > magnitude > MAX_MAGNITUDE) or 
+                    (MIN_LONGITUDE > longitude > MAX_LONGITUDE):
+            continue
 
+        temp.append(featuretype)
+        temp.append(externalID)
+        temp.append(mag)
+        temp.append(place)
+        temp.append(eventDate)
+        temp.append(url)
+        temp.append(tsunami)
+        temp.append(magtype)
+        temp.append(title)
+        temp.append(longitude)
+        temp.append(latitude)
 
-#response.json().get('features')[0]['id']
+        features.append(temp)
+
+    return features
