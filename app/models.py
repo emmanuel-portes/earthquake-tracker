3b95df20db0d7606fcf0bb764a73b7e656466809
Lex Longhorn <emmanuelportes@outlook.com>
2025-10-20 19:22:40 -0400

    implemented dataclasses instead of sql models

--- app/models.py
+++ app/models.py
@@ -1,34 +1,23 @@
-from sqlalchemy import NotNullable
-from sqlalchemy import Integer, Identity, String, Numeric, Date
-from sqlalchemy.orm import Mapped, mapped_column
+from dataclasses import dataclass
+from datetime import date
 
-from app import database
+@dataclass
+class Feature:
+    id: int
+    type: str
+    external_id: str
+    magnitude: float
+    place: str
+    event_date: date
+    url: str
+    tsunami: bool
+    mag_type: str
+    title: str
+    longitude: float
+    latitude: float
 
-class Feature(database.Model):
-    __tablename__ = 'features'
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    type: Mapped[str] = mapped_column(String(8), NotNullable)
-    external_id: Mapped[str] = mapped_column( String(15), unique=True)
-    magnitude: Mapped[float] = mapped_column(Numeric(3,2), NotNullable)
-    place: Mapped[str] = mapped_column( String(100), NotNullable)
-    event_date: Mapped[Date]  = mapped_column( Date, NotNullable)
-    url: Mapped[str] = mapped_column( String(75), NotNullable)
-    tsunami: Mapped[int] = mapped_column( Numeric(1,0), NotNullable)
-    mag_type: Mapped[str] = mapped_column(String(2), NotNullable)
-    title: Mapped[str] = mapped_column(String(100), NotNullable)
-    longitude: Mapped[float] = mapped_column( Numeric(15, 7), NotNullable)
-    latitude: Mapped[float] = mapped_column( Numeric(15, 7), NotNullable)
-
-    def __repr__(self):
-        return f"<Feature {self.external_id}>"
-
-class Comment(database.Model):
-    __tablename__ = "comments"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    commentary: Mapped[str] = mapped_column(String(100), NotNullable)
-    feature_id: Mapped[int] = mapped_column(Integer)
-
-    def __repr__(self):
-        return f"<Comment {self.id}>"
+@dataclass
+class Comment:
+    id: int
+    commentary: str
+    feature_id: str
