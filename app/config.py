a0e134ecd012737207d2ea666c838dfbb58626b9
Lex Longhorn <emmanuelportes@outlook.com>
2025-10-31 16:33:01 -0400

    added new database configuration

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(os.path.dirname(basedir), '.env'))
 
 class Config:
     DEBUG = False
@@ -11,8 +13,14 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'development_tracker.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_DSN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+
 config_by_name = dict(
-    dev=DevelopmentConfig
+    dev=DevelopmentConfig,
+    prod=ProductionConfig
 )
 
 key = Config.SECRET_KEY
