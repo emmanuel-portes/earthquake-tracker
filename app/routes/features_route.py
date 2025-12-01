13aca108fd59bdf0bb82e5c379b53c45791a7a81
Lex Longhorn <emmanuelportes@outlook.com>
2025-12-01 17:37:13 -0400

    Added pagination support for features models

--- app/routes/features_route.py
+++ app/routes/features_route.py
@@ -1,77 +1,54 @@
 from flask import Blueprint, request
 
-from app import database
+from http import HTTPStatus
 
-from sqlalchemy.exc import NoResultFound
+from app.services.features_service import FeatureService
 
-from marshmallow import ValidationError
+from app.exceptions import DataNotProvidedException
 
-from app.models.features_model import Feature
-from app.models.comments_model import Comment
-
-from app.schemas.feature_schema import FeatureSchema
-from app.schemas.comment_schema import CommentSchema
-
+feature_service: FeatureService = FeatureService()
 
 feature = Blueprint('feature', __name__)
 
 @feature.route("/", methods=['GET'])
 def home():
-    return {"message":"hello from earthquake-tracker"}, 200
+    return {"message":"hello from earthquake-tracker"}, HTTPStatus.OK
 
-@feature.route("/api/features", methods=["GET"])
+@feature.get("/api/features")
 def get_features():
-    features: list[Feature] = Feature.query.all()
-    response: dict = FeatureSchema(many=True).dump(features)
-    return {"data": response}, 200
+    page: int = request.args.get("page")
+    number_of_items: int = request.args.get("no_items")
+    response: list = feature_service.get_features(page, number_of_items)
+    return {
+        "status": HTTPStatus.OK, 
+        "data": response
+    }, HTTPStatus.OK
 
 @feature.get("/api/features/<int:feature_id>")
 def get_feature_by_id(feature_id: int):
-    try:
-        feature = Feature.query.filter(Feature.feature_id == feature_id).one()
-    except NoResultFound:
-        return {"message":"Feature could not be found"}, 404
-    response: dict = FeatureSchema().dump(feature)
-    return {"data": response}, 200
+    response: dict = feature_service.get_feature_by_id(feature_id)
+    return {
+        "status": HTTPStatus.OK, 
+        "data": response
+    }, HTTPStatus.OK
 
 @feature.get('/api/features/<external_id>')
 def get_feature_by_external_id(external_id: str):
-    try:
-        feature = Feature.query.filter(Feature.external_id == external_id).one()
-    except NoResultFound:
-        return {"message":"Feature could not be found"}, 404
-    response: dict = FeatureSchema().dump(feature)
-    return {"data": response}, 200
+    response: dict = feature_service.get_feature_by_external_id(external_id)
+    return {
+        "status": HTTPStatus.OK, 
+        "data": response
+    }, HTTPStatus.OK
 
 @feature.post('/api/features')
 def insert_features_comments():
-
-    unparsed_payload = request.get_json()
-
-    if not unparsed_payload:
-        return {'message':'No input data provided'}, 400
+    data: dict = request.get_json()
+    if not data:
+        raise DataNotProvidedException("data not found in payload")
     
-    try:
-        payload: dict = CommentSchema().load(unparsed_payload)
-    except ValidationError as error:
-        return {'message': error.messages}, 422
+    response: dict = feature_service.save_feature_comment(data)
 
-    if (payload.get("commentary") is None) or (payload.get("feature_id") is None):
-        return {'message': 'Comment or feature not availabe'}, 400
-    
-    feature_id: str = payload["feature_id"] 
-    message: str = payload["commentary"]
-
-    feature: Feature = Feature.query.filter(Feature.external_id == feature_id).first()
-
-    if feature is None:
-        return {'message':f'feature: {feature_id} is not registered'}, 400
-    
-    comment: Comment = Comment(commentary=message, feature_id=feature_id)
-
-    database.session.add(comment)
-    database.session.commit()
-
-    response = CommentSchema().dump(Comment.query.get(comment.id))
-
-    return {'message':'Comment succesfully added', "comment": response}, 201
+    return {
+        "status":HTTPStatus.CREATED, 
+        'message':'Comment succesfully added', "data": response
+    }, HTTPStatus.CREATED
--- app/services/features_service.py
+++ app/services/features_service.py
@@ -1,12 +1,49 @@
-from models.features_model import Feature
+from app import database
 
-from schemas.feature_schema import FeatureSchema
+from app.models.features_model import Feature
+from app.models.comments_model import Comment
+
+from app.schemas.feature_schema import FeatureSchema
+from app.schemas.comment_schema import CommentSchema
+
+from app.exceptions import FeatureNotFoundException
 
 class FeatureService:
 
-    def get_features(self) -> list[FeatureSchema]:
-        data: list[Feature] = Feature.query.all()
-        features:list[FeatureSchema] = FeatureSchema(many=True).dump(data)
+    def get_features(self, page:int = 1, number_of_items:int = 10) -> list[dict]:
+        data: list[Feature] = Feature.query.order_by(Feature.event_date.desc()).paginate(page=page, per_page=number_of_items)
+        features:list[FeatureSchema] = FeatureSchema(many=True).dump(data.items)
         return features
     
-    
+    def get_feature_by_id(self, id: int) -> dict:
+        data: Feature | None = Feature.query.filter(Feature.feature_id == id).first()
+        if data is None:
+            raise FeatureNotFoundException(f"Feature of ID: {id} was not found")
+        feature: FeatureSchema = FeatureSchema().dump(data)
+        return feature
+        
+    def get_feature_by_external_id(self, id: str) -> dict:
+        data: Feature | None = Feature.query.filter(Feature.external_id == id).first()
+        if data is None:
+            raise FeatureNotFoundException(f"Feature of external ID: {id} was not found")
+        feature: dict = FeatureSchema().dump(data)
+        return feature
+
+    def save_feature_comment(data:dict[str,str]) -> None:
+        comment: dict = CommentSchema().load(data)
+        feature_id: str = comment["feature_id"] 
+        message: str = comment["commentary"]
+
+        feature: Feature = Feature.query.filter(Feature.external_id == feature_id).first()
+
+        if feature is None:
+            raise FeatureNotFoundException(f"Feature of external ID: {id} was not found")
+        
+        comment: Comment = Comment(commentary=message, feature_id=feature_id)
+
+        database.session.add(comment)
+        database.session.commit()
+
+        response = CommentSchema().dump(Comment.query.get(comment.id))
+
+        return response
